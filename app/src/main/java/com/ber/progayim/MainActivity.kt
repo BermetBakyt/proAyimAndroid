2ef22a0e5ec38878e346df688e8ac90ec91e1a84
Bermet Bakytbekova <bembakyt@gmail.com>
2021-11-08 16:26:19 +0600

    hw-lesson-8 -make text change on click

--- app/src/main/java/com/ber/progayim/MainActivity.kt
+++ app/src/main/java/com/ber/progayim/MainActivity.kt
@@ -1,65 +1,57 @@
 package com.ber.progayim
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var txtView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.e("tag", "Berg: onCreate")
+       super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
 
-        txtView = findViewById(R.id.txtView)
+      txtView = findViewById(R.id.txt_1)
+      val btn1 = findViewById<AppCompatButton>(R.id.btn1)
+      val btn2 = findViewById<AppCompatButton>(R.id.btn2)
+      val btn3 = findViewById<AppCompatButton>(R.id.btn3)
 
-        button1.setOnClickListener {
-            txtView.text = "Red"
-            constraintLayout.setBackgroundColor("#f54248")
-            Toast.makeText(this, "Click 1", Toast.LENGTH_SHORT).show()
-        }
-
-        button2.setOnClickListener {
-            txtView.text = "Yellow"
-            Toast.makeText(this, "Click 2", Toast.LENGTH_LONG).show()
-        }
-
-        button3.setOnClickListener {
-            txtView.text = "Green"
-            Toast.makeText(this, "Click 3", Toast.LENGTH_SHORT).show()
-        }
+      btn1.setOnClickListener(this)
+      btn2.setOnClickListener(this)
+      btn3.setOnClickListener(this)
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.e("tag", "Berg: onCreate")
+    override fun onClick(view: View?) {
+    val msg = when (view?.id) {
+        R.id.btn1 -> "RED"
+        R.id.btn2 -> "GREEN"
+        R.id.btn3 -> "YELLOW"
+        else -> "Unknown"
+    }
+    txtView.text = msg
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.e("tag", "Berg: onCreate")
-    }
 
-    override fun onPause() {
-        super.onPause()
-        Log.e("tag", "Berg: onCreate")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.e("tag", "Berg: onCreate")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.e("tag", "Berg: onCreate")
-    }
 }
+
+
+// Deprecated version of changing text by clicking button
+//override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    setContentView(R.layout.activity_main)
+//}
+//fun onClickEvent(view: View) {
+//    val msg = when (view.id) {
+//        R.id.btn1 -> "Click 1"
+//        R.id.btn2 -> "Click 2"
+//        R.id.btn3 -> "Click 3"
+//        else -> "Unknown"
+//    }
+//    val txtView = findViewById<AppCompatTextView>(R.id.txt_1)
+//    txtView.text = msg
+//}
