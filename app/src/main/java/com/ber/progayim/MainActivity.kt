935930fa1a59e4b2f60895215f46ee7e3673d876
Bermet Bakytbekova <bembakyt@gmail.com>
2021-11-19 06:39:22 +0600

    lesson_19 code

--- app/src/main/java/com/ber/progayim/MainActivity.kt
+++ app/src/main/java/com/ber/progayim/MainActivity.kt
@@ -1,53 +1,34 @@
 package com.ber.progayim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
-
+import com.ber.progayim.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val txt get() = binding.txt1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.txt1
+        setContentView(binding.root)
 
-    }
+//        txt.text = "scadcsc"
+//
+//        binding.txt1.text = "sasasasdsasd"
+        binding.btnGo.setOnClickListener {
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-//        menu?.add("menu 10")
-//        menu?.add("menu 20")
-//        menu?.add("menu 30")
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        when(item.itemId) {
-            R.id.menu_1 -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, Fragment1())
-                    .commit()
-            }
-            R.id.menu_2 -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, Fragment2())
-                    .commit()
-            }
-            R.id.menu_3 -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, Fragment3())
-                    .commit()
-            }
-            else -> {}
         }
-        Toast.makeText(this, item.title, Toast.LENGTH_LONG).show()
-        return super.onOptionsItemSelected(item)
+    binding.apply {
+        txt1.text = "Hello Kotiki"
+        btnGo.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MainActivity2::class.java))
+        }
+    }
+
     }
 }
 
