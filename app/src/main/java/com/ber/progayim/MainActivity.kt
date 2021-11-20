9b1eb463dd3a1dca3e1a043801fab1112659756b
Bermet Bakytbekova <bembakyt@gmail.com>
2021-11-20 06:16:14 +0600

    lesson_21 code

--- app/src/main/java/com/ber/progayim/MainActivity.kt
+++ app/src/main/java/com/ber/progayim/MainActivity.kt
@@ -1,34 +1,33 @@
 package com.ber.progayim
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ber.progayim.databinding.ActivityMainBinding
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    private val txt get() = binding.txt1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.txt1
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-//        txt.text = "scadcsc"
-//
-//        binding.txt1.text = "sasasasdsasd"
-        binding.btnGo.setOnClickListener {
-
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        val layoutManager = LinearLayoutManager(this)
+        val adapter = SimpleAdapter {
+            Toast.makeText(this, "ITEM $it", Toast.LENGTH_SHORT).show()
         }
-    binding.apply {
-        txt1.text = "Hello Kotiki"
-        btnGo.setOnClickListener {
-            startActivity(Intent(this@MainActivity, MainActivity2::class.java))
-        }
-    }
 
+        recycler.layoutManager = layoutManager
+        recycler.adapter = adapter
+        recycler.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
+
+        val list = mutableListOf<String>()
+        for (i in 0..20) {
+            list.add("Item - $i")
+        }
+        adapter.setData(list)
     }
 }
-
---
+++ app/src/main/java/com/ber/progayim/RecyclerView.java
@@ -0,0 +1,5 @@
+package com.ber.progayim;
+
+public class RecyclerView {
+
+}
