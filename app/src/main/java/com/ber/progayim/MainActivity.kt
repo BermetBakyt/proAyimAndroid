b0d8e41b8c99dbb71595eff1577bd9a837e32f36
Bermet Bakytbekova <bembakyt@gmail.com>
2021-11-22 17:08:13 +0600

    lesson_10 hw

--- app/src/main/java/com/ber/progayim/MainActivity.kt
+++ app/src/main/java/com/ber/progayim/MainActivity.kt
@@ -11,32 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val txt = findViewById<TextView>(R.id.txt)
-        val btn = findViewById<Button>(R.id.btn)
-        val btn2 = findViewById<Button>(R.id.btn2)
-
-        var count = 0
-        btn.setOnClickListener {
-            ++count
-            txt.text = count.toString()
-        }
-
-        btn2.setOnClickListener{
-            when(!txt.equals(0)) {
-
-            }
-        }
     }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString("key", "value")
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val value = savedInstanceState.getString("key")
-    }
-
 }
