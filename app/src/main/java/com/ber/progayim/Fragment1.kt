ca248a9d95d98d614b0cf3a72393878bd1fc8488
Bermet Bakytbekova <bembakyt@gmail.com>
2021-11-24 18:03:09 +0600

    lesson_23

---
+++ app/src/main/java/com/ber/progayim/Fragment1.kt
@@ -0,0 +1,8 @@
+package com.ber.progayim
+
+import androidx.fragment.app.Fragment
+
+class Fragment1 : Fragment(R.layout.fragment_1) {
+
+
+}
--- app/src/main/java/com/ber/progayim/MainActivity.kt
+++ app/src/main/java/com/ber/progayim/MainActivity.kt
@@ -2,67 +2,32 @@ package com.ber.progayim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.widget.AppCompatTextView
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private var workingsTV = findViewById<TextView>(R.id.workingsTV)
-    private var resultsTV = findViewById<TextView>(R.id.resultsTV)
-    private var canAddOperation = false
-    private var canAddDecimal = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
-    fun numberAction(view: View) {
-        if(view is Button) if (view.text == ".") {
-            workingsTV.append(view.text)
-
-            canAddDecimal = false
-        } else
-            workingsTV.append(view.text)
-            canAddDecimal = true
-    }
-
-    fun operationAction(view: View) {
-        if(view is Button && canAddOperation) {
-            workingsTV.append(view.text)
-            canAddOperation = false
-            canAddDecimal = true
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        val layoutManager = LinearLayoutManager(this)
+        val adapter = SimpleAdapter {
+            Toast.makeText(this, "ITEM $it", Toast.LENGTH_SHORT).show()
         }
-    }
 
-    fun backSpaceAction(view: View) {
-        val length = workingsTV.length()
-        if (length > 0) {
-            workingsTV.text = workingsTV.text.subSequence(0, length - 1)
+        recycler.layoutManager = layoutManager
+        recycler.adapter = adapter
+        recycler.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
+
+        val list = mutableListOf<String>()
+        for (i in 0..20) {
+            list.add("Item - $i")
         }
-    }
 
-    fun allClearAction(view: View) {
-        workingsTV.text = ""
-        resultsTV.text = ""
-    }
-    fun equalsAction(view: View) {
-        resultsTV.text = calculateResults()
-    }
-
-    private fun calculateResults(): String {
-
-        return ""
-    }
-    private fun digitOperators() :MutableList<Any>{
-        val list = mutableListOf<Any>()
-        var currentDigit =""
-        for(character in workingsTV.text)
-                if (character.isDigit() || character == "." += character)
-                    currentDigit = character
-                else {
-                    list.add(currentDigit.toFloat())
-            }
+        adapter.setData(list)
     }
 }
