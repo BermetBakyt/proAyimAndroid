4102cc31cf02727204984ddae51968a3084844c8
Bermet Bakytbekova <bembakyt@gmail.com>
2021-11-24 17:05:48 +0600

    lesson_10 fix bugs

--- app/src/main/java/com/ber/progayim/MainActivity.kt
+++ app/src/main/java/com/ber/progayim/MainActivity.kt
@@ -2,45 +2,67 @@ package com.ber.progayim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var txt: TextView
-    private lateinit var btn: Button
-    private var count: Int = 0
 
+    private var workingsTV = findViewById<TextView>(R.id.workingsTV)
+    private var resultsTV = findViewById<TextView>(R.id.resultsTV)
+    private var canAddOperation = false
+    private var canAddDecimal = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val txt = findViewById<TextView>(R.id.txt)
-        val btn = findViewById<Button>(R.id.btn)
-        val btn2 = findViewById<Button>(R.id.btn2)
+    }
+    fun numberAction(view: View) {
+        if(view is Button) if (view.text == ".") {
+            workingsTV.append(view.text)
 
-        var count = 0
-        btn.setOnClickListener {
-            ++count
-            txt.text = count.toString()
+            canAddDecimal = false
+        } else
+            workingsTV.append(view.text)
+            canAddDecimal = true
+    }
+
+    fun operationAction(view: View) {
+        if(view is Button && canAddOperation) {
+            workingsTV.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
         }
+    }
 
-        btn2.setOnClickListener{
-            if(!txt.equals(0)) {
-                txt.text = 0.toString()
+    fun backSpaceAction(view: View) {
+        val length = workingsTV.length()
+        if (length > 0) {
+            workingsTV.text = workingsTV.text.subSequence(0, length - 1)
+        }
+    }
+
+    fun allClearAction(view: View) {
+        workingsTV.text = ""
+        resultsTV.text = ""
+    }
+    fun equalsAction(view: View) {
+        resultsTV.text = calculateResults()
+    }
+
+    private fun calculateResults(): String {
+
+        return ""
+    }
+    private fun digitOperators() :MutableList<Any>{
+        val list = mutableListOf<Any>()
+        var currentDigit =""
+        for(character in workingsTV.text)
+                if (character.isDigit() || character == "." += character)
+                    currentDigit = character
+                else {
+                    list.add(currentDigit.toFloat())
             }
-        }
     }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString("count", count.toString())
-
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val count = savedInstanceState.getString("count")
-    }
-
 }
