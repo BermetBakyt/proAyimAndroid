77316e9e3a84c6bc7e32d517ed1d569351b1864e
Bermet Bakytbekova <bembakyt@gmail.com>
2021-12-05 00:57:29 +0600

    hw 9 counter finish

--- app/src/main/java/com/ber/progayim/MainActivity.kt
+++ app/src/main/java/com/ber/progayim/MainActivity.kt
@@ -7,40 +7,35 @@ import android.widget.TextView
 import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var txt: TextView
-    private lateinit var btn: Button
-    private var count: Int = 0
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val txt = findViewById<TextView>(R.id.txt)
-        val btn = findViewById<Button>(R.id.btn)
-        val btn2 = findViewById<Button>(R.id.btn2)
+        val btnIncrement = findViewById<Button>(R.id.btn)
+        val resetBtn = findViewById<Button>(R.id.btn2)
 
         var count = 0
-        btn.setOnClickListener {
-            ++count
-            txt.text = count.toString()
+        btnIncrement.setOnClickListener {
+            count++
+            txt.text = "$count"
         }
 
-        btn2.setOnClickListener{
-            if(!txt.equals(0)) {
-                txt.text = 0.toString()
-            }
+        resetBtn.setOnClickListener{
+            count = 0
+            txt.text = "$count"
+        }
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt("key")
+            txt.text = "$count"
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString("count", count.toString())
+        outState.putInt("key", count)
 
     }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val count = savedInstanceState.getString("count")
-    }
-
 }
