37503374c81abcf2741f327663d31e8ae7758594
Kudaybergen <tuleubekov.kas@gmail.com>
2021-12-14 09:11:46 +0600

    commit: hw-15

--- app/src/main/java/com/ber/progayim/MainActivity.kt
+++ app/src/main/java/com/ber/progayim/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
+// внимательнее к условиям задачи
+
 class MainActivity : AppCompatActivity(), OnClickListener {
     private lateinit var image: ImageView
     private lateinit var layout: ConstraintLayout
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
 
     }
 
-     fun onClickListener(image: ImageView) {
+     fun onClickListener(image: ImageView) { // не неужно передавать вьюшку)
         supportFragmentManager.findFragmentById(R.id.container2) as? Fragment2
          fragment2.setValue(image)
     }
